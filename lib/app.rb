d702023a9d246849541d4af8d590e63e515692fc
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-07-27 14:17:22 +0100

    Produce visits graph from stub data
    Produce a first draft of the d3.js line graph for visits.
    
    @author Jake
    @author Rob
    
    commit 381806d3c554ae789ff8e5f935318106072a55e9
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jul 27 14:12:09 2012 +0100
    
        Set y-scale top limit to the next round million.
    
        The top of the y-axis was sometimes being pushed off the top of the
        canvas. To prevent this the top of the y scale has been set to the next
        round million after the top visits value. This is the same as for the
        labels.
    
        @author Jake
        @author Rob
    
    commit 3686e6d7636529dd884caccda1f6ab3a877b7534
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jul 27 14:04:16 2012 +0100
    
        Tidy up the code.
    
        @author Jake
        @author Rob
    
    commit 99643f638065f223affb1e676bae587fbc61a259
    Author: Jake Choules <jakechoules@googlemail.com>
    Date:   Fri Jul 27 13:46:58 2012 +0100
    
        Alter svg margins and axis positions.
    
        Decrease top, left and right margins and move y-axis left.
    
        @author Jake
        @author Rob
    
    commit 62adeafdbde7eb93ae4e652aecca396357484452
    Author: Jake Choules <jakechoules@googlemail.com>
    Date:   Fri Jul 27 12:15:30 2012 +0100
    
        Remove x- and y-axis ticks.
    
        @author Jake
        @author Rob
    
    commit bb580ebd3fa3639597aba137a1f25590a398be45
    Author: Jake Choules <jakechoules@googlemail.com>
    Date:   Fri Jul 27 11:55:05 2012 +0100
    
        Add dynamic calculation of y-axis tick positions.
    
        Display y-axis tick marks every million until just above the maximum value.
    
        @author Jake
        @author Rob
    
    commit 04b2eca5dd7cdbfa5d23ad55970f7fecc10a281f
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jul 27 11:26:28 2012 +0100
    
        Change the y-axis label to remove m for zero.
    
    commit a4819e127510f2ad1dc7f26e0454c604e861e63b
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jul 27 11:06:22 2012 +0100
    
        More work around label positions and dimensions.
    
    commit 838d897170986239ce35c4fdc48be683439e773c
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jul 27 11:05:14 2012 +0100
    
        Remove accidental patch from d3.js
    
        Ooops, I didn't mean to commit that.
    
    commit c7c70ce5fc20ee2477d6d69600ff02cd9e090d9b
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Thu Jul 26 19:24:25 2012 +0100
    
        Add first pass visits module.
    
        @author Seema
        @author Jake
        @author Rob
    
    commit b02ed542686e39a171b0c4658f55d1e2acbeb7b4
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Thu Jul 26 19:23:14 2012 +0100
    
        Add 3rd party javascript libraries
    
        JQuery 1.7.2 for DOM manipulation
        d3.js for graphs
    
        @author Seema
        @author Jake
        @author Rob
    
    commit 47c77febf56580f2e639b3012eb38f12ae5bfc95
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Thu Jul 26 19:22:24 2012 +0100
    
        Add dummy json action for visits
    
        @author Seema
        @author Jake
        @author Rob

--- lib/app.rb
+++ lib/app.rb
@@ -21,4 +21,26 @@ class App < Sinatra::Base
   get "/engagement" do
     erb :engagement
   end
+
+  get "/visits.json" do
+    content_type :json
+    start_date = Date.today.prev_month(6)
+    #start_date = Date.new(start_date.year, start_date.month, 1)
+    end_date   = Date.today
+    response = {}
+    response["govuk"] = (start_date..end_date).step(7).map do |date|
+      {"date" => date, "visits" => 1000000 + (rand * 4000000).to_i }
+    end
+    response["directgov"] = (start_date..end_date).step(7).map do |date|
+      {"date" => date, "visits" => 500000 + (rand * 2500000).to_i }
+    end
+    response["businesslink"] = (start_date..end_date).step(7).map do |date|
+      {"date" => date, "visits" => 300000 + (rand * 700000).to_i }
+    end
+    response.to_json
+  end
+
+  get "/visits" do
+    erb :visits
+  end
 end
