a5e066c4f8ef55c4516c6430a63fd7b75fbf85b6
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-01 18:39:13 +0100

    Make release script more readable.
    Split the big ssh command into three distinct tasks to make it easier to
    read.

--- release.sh
+++ release.sh
@@ -12,4 +12,9 @@ fi
 HOST="deploy@datainsight.alphagov.co.uk"
 
 scp datainsight-web-$VERSION.zip $HOST:/srv/datainsight-web/packages
-ssh $HOST "mkdir /srv/datainsight-web/release/$VERSION; unzip -o /srv/datainsight-web/packages/datainsight-web-$VERSION.zip -d /srv/datainsight-web/release/$VERSION; rm /srv/datainsight-web/current; ln -s /srv/datainsight-web/release/$VERSION/ /srv/datainsight-web/current; sudo service datainsight-web restart"
+# deploy
+ssh $HOST "mkdir /srv/datainsight-web/release/$VERSION; unzip -o /srv/datainsight-web/packages/datainsight-web-$VERSION.zip -d /srv/datainsight-web/release/$VERSION;"
+# link
+ssh $HOST "rm /srv/datainsight-web/current; ln -s /srv/datainsight-web/release/$VERSION/ /srv/datainsight-web/current;"
+# restart
+ssh $HOST "sudo service datainsight-web restart"
