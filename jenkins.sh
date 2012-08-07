755ffa80681e70be9764613624d86ddcd121e07f
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-07 11:34:45 +0100

    Remove ci setup from jasmine tests.
    Jasmine tests do not go through the test reporter yet. They will cause
    the build to fail if they do not pass but they will not update the
    graph.

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,5 @@
 set -e
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
-# TODO: does this need to check return values?
+bundle exec rake jasmine:phantom:ci --trace
 bundle exec rake ci:setup:rspec spec --trace
-bundle exec rake ci:setup:rspec jasmine:phantom:ci --trace
