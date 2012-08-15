809e0fe10aea0977701c99bbf56aff8fcdfc8e2e
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-08-15 16:52:19 +0100

    Change tests to conform to the interface
    Author: @jthoenes
    Author: @pbadenski

--- spec/javascripts/reachSpec.js
+++ spec/javascripts/reachSpec.js
@@ -2,11 +2,11 @@ describe("reach", function() {
     describe("axes labels", function() {
         describe("tick values for y axes", function() {
             it("should produce correct tick values", function() {
-                expect(get_tick_values([2100, 3400, 1000, 2300, 5000, 6000, 3000], 3)).toEqual([0, 3000, 6000]);
+                expect(get_tick_values(6000, 3)).toEqual([0, 3000, 6000]);
             });
 
             it("should produce correct tick values", function() {
-                expect(get_tick_values([2100, 3400, 1000, 2300, 80000, 6000, 3000, 60000, 34900], 6)).toEqual([ 0, 16000, 32000, 48000, 64000, 80000 ]);
+                expect(get_tick_values(80000, 6)).toEqual([ 0, 16000, 32000, 48000, 64000, 80000 ]);
             });
         });
     });
