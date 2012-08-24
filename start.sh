e6a02b24b35a724274df89c19948f7308463c602
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-08-24 08:28:23 +0100

    Whenever should update crontab using fixed id
    Author: @pbadenski

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ ANSI_RESET="\033[0m"
 echo -e "${ANSI_YELLOW}Installing dependencies${ANSI_RESET}"
 bundle install --path vendor --local
 
-bundle exec whenever --set "port=$PORT" --update-crontab
+bundle exec whenever --set "port=$PORT" --update-crontab datainsight-web
 echo -e "${ANSI_YELLOW}Starting application server${ANSI_RESET}"
 bundle exec unicorn --port $PORT
