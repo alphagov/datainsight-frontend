7d326014cf5f8877c069e2a6efc8d753756846a2
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-08-31 16:45:10 +0100

    PhantomJS - correct resolving path dynamically
    Author: @jthoenes
    Author: @pbadenski

--- bin/save_graphs_as_images.rb
+++ bin/save_graphs_as_images.rb
@@ -8,7 +8,8 @@ def save_as_image(route, output_filename, div_selector)
   url = "http://localhost:#{PORT}/#{route}"
   output_location = "#{OUTPUT_DIR}/#{output_filename}.png"
   LOGGER.info("Generating image for #{url}")
-  system("phantomjs bin/rasterize.js #{url} #{output_location} '#{div_selector}'")
+  phantomjs_bin = `which phantomjs`.strip
+  system("#{phantomjs_bin} bin/rasterize.js #{url} #{output_location} '#{div_selector}'")
   case $?.exitstatus
     when 0
       # do nothing
