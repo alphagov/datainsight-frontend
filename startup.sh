0e3f0b5a564877d0445e96ac2729106519a41eff
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-09-03 13:28:51 +0100

    Startup script for upstart
    Author: @jthoenes
    Author: @pbadenski

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+PORT=3027
+
+echo -e "Installing dependencies"
+bundle install --path vendor --local
+
+bundle exec whenever --set "port=$PORT" --update-crontab datainsight-web
+echo -e "Starting application server"
+bundle exec unicorn --port $PORT
