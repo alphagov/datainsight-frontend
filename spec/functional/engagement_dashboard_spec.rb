c8ebdfa6616fa38ba9160c6342f7535041c69bb4
Yael Sembira <sembirayael@gmail.com>
2012-09-05 14:18:10 +0100

    Removed an unused trust test.
    Author: @sembirayael

--- spec/functional/engagement_dashboard_spec.rb
+++ spec/functional/engagement_dashboard_spec.rb
@@ -3,11 +3,11 @@ require_relative "test_helper"
 describe "Engagement Dashboard" do
   include CommonSetup
 
-  it "should do something" do
-    visit "/trust"
-    # d3.js should insert an svg element
-    # strange xpath syntax is used to get around fact that svg elements are not visible to xpath normally.
-    # see: http://stackoverflow.com/questions/5433825/having-trouble-using-capybara-and-selenium-to-find-an-svg-tag-on-a-page
-    page.should have_xpath("//*[@class='graph-section-block']//*[name()='svg']")
-  end
+  #it "should do something" do
+  #  visit "/trust"
+  #  # d3.js should insert an svg element
+  #  # strange xpath syntax is used to get around fact that svg elements are not visible to xpath normally.
+  #  # see: http://stackoverflow.com/questions/5433825/having-trouble-using-capybara-and-selenium-to-find-an-svg-tag-on-a-page
+  #  page.should have_xpath("//*[@class='graph-section-block']//*[name()='svg']")
+  #end
 end
