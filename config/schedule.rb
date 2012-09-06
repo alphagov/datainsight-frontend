6a019dd29490cc6c8914b7ee8395e4662e3b289f
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-06 17:43:29 +0100

    Adding out and error log for cronjobs.
    author: @jthoenes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,12 @@
+def cron_command
+  root_path = File.expand_path(File.dirname(__FILE__) + "/../")
+
+  "cd #{root_path} && RACK_ENV=production bundle exec ruby bin/save_graphs_as_images.rb" +
+      " >> #{root_path}/log/cron.out.log 2>> #{root_path}/log/cron.err.log"
+end
+
 job_type :custom_script, ":task :port"
 
 every 5.minutes do
-  custom_script "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec ruby bin/save_graphs_as_images.rb"
+  custom_script cron_command
 end
