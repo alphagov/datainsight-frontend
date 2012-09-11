afe86a34773cb1f43af41c757bbe75be159019b8
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-09-11 13:14:16 +0100

    Log exceptions when Capybara tests are run
    Author: @pbadenski

--- spec/functional/test_helper.rb
+++ spec/functional/test_helper.rb
@@ -9,6 +9,10 @@ require "rspec/core/shared_context"
 RSpec.configure do |config|
   config.include Capybara::DSL
   Capybara.default_driver = :poltergeist
+  Capybara.server do |app, port|
+    require 'rack/handler/webrick'
+    Rack::Handler::WEBrick.run(app, :Port => port, :AccessLog => [], :Logger => WEBrick::Log::new("log/capybara_test.log"))
+  end
 end
 
 module CommonSetup
