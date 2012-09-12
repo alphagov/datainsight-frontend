e6b4c1eb93af4824c6d67c398bca7f58bdfd42ba
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-12 16:57:23 +0100

    Workers are not the problem
    author: @jthoenes

--- config/unicorn.rb
+++
@@ -1,6 +0,0 @@
-worker_processes 10
-stdout_path "#{ENV.fetch('GOVUK_APP_LOGROOT')}/app.out.log"
-stderr_path "#{ENV.fetch('GOVUK_APP_LOGROOT')}/app.err.log"
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{ENV.fetch('GOVUK_APP_ROOT')}/Gemfile"
-end
