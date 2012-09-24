9035203e066af0a1afd93f1a0ec6ab5a7d26a8d8
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-24 15:13:31 +0100

    Remove old package, deployment and start scripts.
    author: @pbadenski
    author: @jthoenes

--- test.sh
+++
@@ -1,6 +0,0 @@
-for service in `cat lib/api.rb  | grep 'transport("' | sed -e 's/transport("\(.*\)").get("\(.*\)").data/\1\2/'`; do
-  response_code=`curl -o /dev/null -s -w "%{http_code}" $service`
-  if [ $response_code -ne "200" ]; then
-    echo "Service not working properly: $service, responded with: $response_code"
-  fi
-done
