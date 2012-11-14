18130a0c790ea3fbcad87c0bbd7ddeda2a1a07fb
Max Fliri <mfliri@thoughtworks.com>
2012-11-14 14:46:38 +0000

    Tabular numbers in performance dashboard.
    - @maxfliri

--- config/feature_toggles.rb
+++ config/feature_toggles.rb
@@ -4,6 +4,7 @@ class FeatureToggles
     if env.to_sym == :development or env.to_sym == :test
       # Add feature toggles here
       # FEATURE[:feature_name] = true
+      FEATURE[:use_tabular_numbers] = true
     end
     FEATURE[:show_weekly_visitors_in_narrative] = true
   end
