93042a5f92aa80b9c34755777fd26f13cae5ef8d
Gareth Rogers <grogers@thoughtworks.com>
2012-11-15 11:21:05 +0000

    Adds blank rails project with rspec and no database
    - @gtrogers
    - @mfliri

---
+++ rails/datainsight-frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatainsightFrontend::Application.config.secret_token = '81081c80458218c90f1e5d02ddf0826c00a349bed9faa07237a17b8065efcf806fc663cc54a569af86f534f7a8fc373e8a200feb8823a4cdd1d0b1e790c70b2a'
