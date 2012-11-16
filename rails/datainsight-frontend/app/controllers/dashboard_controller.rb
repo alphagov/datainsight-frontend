022b838aadfe762106fa1ccab53c08dceb704e49
Gareth Rogers <grogers@thoughtworks.com>
2012-11-16 17:03:53 +0000

    Dashboard now works.
    - @maxfliri
    - @gtrogers

--- rails/datainsight-frontend/app/controllers/dashboard_controller.rb
+++ rails/datainsight-frontend/app/controllers/dashboard_controller.rb
@@ -1,5 +1,3 @@
-
-
 class DashboardController < ApplicationController
 
   def api(config)
@@ -31,41 +29,37 @@ class DashboardController < ApplicationController
 
 
   def index
-
+    @narrative = get_narrative
   end
 
   def visits
     respond_to do |format|
-      format.html do
-        # get weekly visits
-      end
+      format.html
+      format.json { serve_json("weekly_visits") }
       format.png { serve_image("visits") }
     end
   end
 
   def unique_visitors
     respond_to do |format|
-      format.html do
-        # get weekly visits
-      end
+      format.html
+      format.json { serve_json("weekly_visitors") }
       format.png { serve_image("unique-visitors") }
     end
   end
 
   def format_success
     respond_to do |format|
-      format.html do
-        # get weekly visits
-      end
+      format.html
+      format.json { serve_json("format_success") }
       format.png { serve_image("format-success") }
     end
   end
 
   def hourly_traffic
     respond_to do |format|
-      format.html do
-        # get weekly visits
-      end
+      format.html
+      format.json { serve_json("hourly_traffic") }
       format.png { serve_image("hourly-traffic") }
     end
   end
@@ -76,7 +70,21 @@ class DashboardController < ApplicationController
   end
 
   def narrative
-    @narrative = get_narrative
+    respond_to do |format|
+      format.html { @narrative = get_narrative }
+      format.json { serve_json("narrative") }
+    end
+  end
+
+  def serve_json(endpoint)
+    request_url = "#{request.scheme}://#{request.host}#{request.path}".chomp(".json")
+    api = api(DataInsightSettings)
+    result = api.send(endpoint.to_sym, request_url)
+    if result == :error
+      render status: 500
+    else
+      render json: result
+    end
   end
 
 end
