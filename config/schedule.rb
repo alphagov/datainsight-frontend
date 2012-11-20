63e4ca184c0d2c6802fca346f12dac308b1d566a
Max Fliri <mfliri@thoughtworks.com>
2012-11-20 14:43:23 +0000

    Unused RACK_ENV variable removed from image generation scripts
    - @maxfliri

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ set :output, {
     :standard => "#{root_path}/log/cron.out.log",
     :error => "#{root_path}/log/cron.err.log"
 }
-job_type :ruby, "cd :path && RACK_ENV=:environment bundle exec ruby :task :port :output"
+job_type :ruby, "cd :path && bundle exec ruby :task :port :output"
 
 every 1.hour, :at => "00:31" do
   ruby "bin/save_graphs_as_images.rb", :environment => 'production', :port => 3027
--- lib/tasks/images.rake
+++ lib/tasks/images.rake
@@ -1,8 +1,7 @@
 namespace :images do
   desc "Generate all images initially"
   task :generate do
-    rack_env = ENV.fetch('RACK_ENV', 'development')
     root_path = File.expand_path(File.dirname(__FILE__) + "/../../")
-    sh %{cd #{root_path} && RACK_ENV=#{rack_env} bundle exec bin/save_graphs_as_images.rb 3027}
+    sh %{cd #{root_path} && bundle exec bin/save_graphs_as_images.rb 3027}
   end
 end
