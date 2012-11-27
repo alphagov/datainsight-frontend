3beee9b30cb1e46f284dcee11003b5826629a968
Max Fliri <mfliri@thoughtworks.com>
2012-11-27 14:40:49 +0000

    Show 5 empty rows for most visited policies
    - @maxfliri
    - @robyoung

--- spec/functional/inside_government/most_visited_policies_spec.rb
+++ spec/functional/inside_government/most_visited_policies_spec.rb
@@ -8,4 +8,10 @@ describe "Most Visited Policies" do
     page.find("#most-visited-policies-module h2").text.should == "Most visited policies"
   end
 
+  it "should show 5 most visited policies" do
+    visit "/performance/dev/inside-government"
+
+    page.all("#most-visited-policies-module table tr").count.should == 5
+  end
+
 end
