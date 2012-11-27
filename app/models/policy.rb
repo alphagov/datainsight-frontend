0101555709ae3214cfb0722c22bf7152756a53ff
Max Fliri <mfliri@thoughtworks.com>
2012-11-27 17:31:13 +0000

    Show department and update date (Most visited policies)
    - @maxfliri
    - @robyoung

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,7 +1,9 @@
 class Policy
-  attr_reader :title
+  attr_reader :title, :department, :update_date
 
   def initialize(properties = {})
     @title = properties[:title]
+    @department = properties[:department]
+    @update_date = Date.iso8601(properties[:updated_at])
   end
 end
--- spec/functional/inside_government/most_visited_policies_spec.rb
+++ spec/functional/inside_government/most_visited_policies_spec.rb
@@ -15,27 +15,37 @@ def policies_json
 		"data":[
 			{
 				"policy": {
-					"title": "Most visited policy"
+					"title": "Most visited policy",
+          "department": "ABC",
+          "updated_at": "2012-11-25T16:00:07+00:00"
 				}
       },
       {
 				"policy": {
-					"title": "Second most visited policy"
+					"title": "Second most visited policy",
+          "department": "DEF",
+          "updated_at": "2012-11-25T16:00:07+00:00"
 				}
       },
       {
 				"policy": {
-					"title": "#3 most visited policy"
+					"title": "#3 most visited policy",
+          "department": "GHI",
+          "updated_at": "2012-11-24T16:00:07+00:00"
 				}
       },
       {
 				"policy": {
-					"title": "#4 most visited policy"
+					"title": "#4 most visited policy",
+          "department": "JKL",
+          "updated_at": "2012-11-23T16:00:07+00:00"
 				}
       },
       {
 				"policy": {
-					"title": "#5 most visited policy"
+					"title": "#5 most visited policy",
+          "department": "MNO",
+          "updated_at": "2012-11-25T16:00:07+00:00"
 				}
 			}
 		]
@@ -69,7 +79,11 @@ describe "Most Visited Policies" do
     page.all("#most-visited-policies-module table tr").count.should == 5
 
     page.all("#most-visited-policies-module table tr .policy-title").first.text.should == "Most visited policy"
+    page.all("#most-visited-policies-module table tr .policy-department").first.text.should == "ABC"
+    page.all("#most-visited-policies-module table tr .policy-updated-at").first.text.should == "Updated 25 November 2012"
+
     page.all("#most-visited-policies-module table tr .policy-title").second.text.should == "Second most visited policy"
+
     page.all("#most-visited-policies-module table tr .policy-title")[2].text.should == "#3 most visited policy"
     page.all("#most-visited-policies-module table tr .policy-title")[3].text.should == "#4 most visited policy"
     page.all("#most-visited-policies-module table tr .policy-title")[4].text.should == "#5 most visited policy"
