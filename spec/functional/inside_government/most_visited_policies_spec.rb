d354e71c6e7841e7b73366223ac6b25d13fc8ee8
Max Fliri <mfliri@thoughtworks.com>
2012-11-28 10:52:33 +0000

    Test refactored to a less verbose setup
    - @maxfliri

--- spec/functional/inside_government/most_visited_policies_spec.rb
+++ spec/functional/inside_government/most_visited_policies_spec.rb
@@ -1,84 +1,64 @@
 require "functional/test_helper"
 
-def policies_json
-  <<-HERE
-{
-	"response_info":{
-		"status":"ok"
-	},
-	"id":"https://www.gov.uk/performance/dashboard/visits.json",
-	"web_url":"https://www.gov.uk/performance/dashboard/visits",
-	"details":{
-		"source":["Google Analytics","Celebrus","Omniture"],
-		"start_at":"2012-05-20",
-		"end_at":"2012-06-03",
-		"data":[
-			{
-				"policy": {
-					"title": "Most visited policy",
-          "department": "ABC",
-          "updated_at": "2012-11-25T16:00:07+00:00"
-				},
-        "visits": 567000
+def most_visited_policies_body(overrides)
+  policy_visits_entity = {
+      "response_info" => {
+          "status" => "ok"
       },
-      {
-				"policy": {
-					"title": "Second most visited policy",
-          "department": "DEF",
-          "updated_at": "2012-11-25T16:00:07+00:00"
-				},
-        "visits": 1
+      "id" => "https://www.gov.uk/performance/dashboard/visits.json",
+      "web_url" => "https://www.gov.uk/performance/dashboard/visits",
+      "details" => {
+          "source" => ["Google Analytics", "Celebrus", "Omniture"],
+          "start_at" => "2012-05-20",
+          "end_at" => "2012-06-03",
+          "data" => []
       },
-      {
-				"policy": {
-					"title": "#3 most visited policy",
-          "department": "GHI",
-          "updated_at": "2012-11-24T16:00:07+00:00"
-				},
-        "visits": 1
+      "updated_at" => "2012-11-26T16:00:07+00:00"
+  }
+
+  policy_defaults = {
+      "policy" => {
+          "title" => "Default policy title",
+          "department" => "ZZZ",
+          "updated_at" => "2000-01-01T12:00:00+00:00"
       },
-      {
-				"policy": {
-					"title": "#4 most visited policy",
-          "department": "JKL",
-          "updated_at": "2012-11-23T16:00:07+00:00"
-				},
-        "visits": 1
-      },
-      {
-				"policy": {
-					"title": "#5 most visited policy",
-          "department": "MNO",
-          "updated_at": "2012-11-25T16:00:07+00:00"
-				},
-        "visits": 1
-			}
-		]
-	},
-	"updated_at":"2012-11-26G16:00:07+00:00"
-}
-  HERE
+      "visits" => 0
+  }
+
+  overrides.each do |policy_data|
+    policy_visits_entity["details"]["data"] << policy_defaults.deep_merge(policy_data)
+  end
+
+  policy_visits_entity.to_json
 end
+
+def serve_most_visited_policies(policy_data)
+  FakeWeb.register_uri(
+      :get,
+      "#{Settings.api_urls['inside_government_base_url']}/most-visited-policies",
+      :body => most_visited_policies_body(policy_data))
+end
+
 
 describe "Most Visited Policies" do
 
-  it "should show most visited policies" do
-    FakeWeb.register_uri(
-        :get,
-        "#{Settings.api_urls['inside_government_base_url']}/most-visited-policies",
-        :body => policies_json)
+  before :each do
+    serve_most_visited_policies([
+        {"policy" => {"title" => "Most visited policy", "department" => "ABC", "updated_at" => "2012-11-25T16:00:07+00:00"}, "visits" => 567000},
+        {"policy" => {"title" => "Second most visited policy"}},
+        {"policy" => {"title" => "#3 most visited policy"}},
+        {"policy" => {"title" => "#4 most visited policy"}},
+        {"policy" => {"title" => "#5 most visited policy"}}
+    ])
+  end
 
+  it "should show most visited policies" do
     visit "/performance/dev/inside-government"
 
     page.find("#most-visited-policies-module h2").text.should == "Most visited policies"
   end
 
   it "should show 5 most visited policies" do
-    FakeWeb.register_uri(
-        :get,
-        "#{Settings.api_urls['inside_government_base_url']}/most-visited-policies",
-        :body => policies_json)
-
     visit "/performance/dev/inside-government"
 
     page.all("#most-visited-policies-module table tr").count.should == 5
