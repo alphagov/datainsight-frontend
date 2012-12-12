67328c50ad37d39912387365e6bcb6c7c6d6b8b0
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-12 12:24:04 +0000

    Add feature toggle for inside gov dashboard.
    @pbadenski
     @robyoung

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,13 @@ DataInsightFrontend::Application.routes.draw do
     match "dashboard/format-success" => "dashboard#format_success", via: :get
     match "dashboard/hourly-traffic" => "dashboard#hourly_traffic", via: :get
 
-    if Rails.env.development? or Rails.env.test?
+    if Settings.feature_toggles[:inside_government_dashboard]
       match "dashboard/government" => "inside_government#index", via: :get
       match "dashboard/government/format-success" => "inside_government#format_success", via: :get
       match "dashboard/government/most-entered-policies" => "inside_government#most_entered_policies", via: :get
       match "dashboard/government/visitors/weekly" => "inside_government#visitors_weekly", via: :get
       match "dashboard/government/narrative" => "inside_government#narrative", via: :get
+      match "dashboard/government" => "inside_government#index", via: :get
     end
-
-
-    match "dashboard/government" => "inside_government#index", via: :get
-
   end
-
 end
