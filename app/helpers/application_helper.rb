fd8a15d65df853f031c7e8c1f501f8615d1e95f8
Max Fliri <mfliri@thoughtworks.com>
2012-12-13 16:42:45 +0000

    Fit policies table into its cell.
    - @maxfliri

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     link_to text, options, class: (current_page?(options) ? 'current' : '')
   end
 
+  def allow_line_break_after_slash(text)
+    h(text).gsub("/", "/&#8203;").html_safe
+  end
+
 end
