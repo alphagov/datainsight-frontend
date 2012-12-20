c43fa61bc3f13ffb1a5505023b81da0ff24d1faf
Gareth Rogers <grogers@thoughtworks.com>
2012-12-20 16:24:00 +0000

    Moves point.js -> geometry... inclues vendors in jasmine setup

--- app/assets/javascripts/helpers/geometry.js
+++ app/assets/javascripts/helpers/geometry.js
@@ -46,3 +46,30 @@ GOVUK.Insights.clamp = function (value, min, max) {
     if (value < min) return min;
     return value;
 };
+
+GOVUK.Insights.point = function(x, y) {
+
+    var _x, _y;
+
+    if (jQuery.isArray(x)) {
+        _x = x[0];
+        _y = x[1];
+    } else {
+        _x = x;
+        _y = y;
+    }
+
+    return {
+        x: function() { return _x; },
+
+        y: function() { return _y; },
+
+        distanceFrom: function(otherPoint) {
+            return Math.sqrt(Math.pow(_x - otherPoint.x(), 2) + Math.pow(_y - otherPoint.y(), 2));
+        },
+
+        toString: function() {
+            return "point {" + _x + "," + _y + "}";
+        }
+    }
+};
