01eaeaa00d8a981d06b7b3214a7824c4524296a0
Gareth Rogers <grogers@thoughtworks.com>
2012-12-20 16:26:29 +0000

    Moves point.js tests to spec/helpers

--- spec/javascripts/specs/helpers/geometrySpec.js
+++ spec/javascripts/specs/helpers/geometrySpec.js
@@ -1,4 +1,4 @@
-describe("geometry helpers", function() {
+describe("geometry helpers", function () {
     describe("Collision Rectangle", function () {
         it("should initialize from a SVGRectangle", function () {
             var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
@@ -64,10 +64,39 @@ describe("geometry helpers", function() {
 
     describe("clamp function", function () {
         it("should clamp a a value to a given range", function () {
-            expect(GOVUK.Insights.clamp(10,4,6)).toBe(6);
-            expect(GOVUK.Insights.clamp(1,4,6)).toBe(4);
-            expect(GOVUK.Insights.clamp(10,4,16)).toBe(10);
+            expect(GOVUK.Insights.clamp(10, 4, 6)).toBe(6);
+            expect(GOVUK.Insights.clamp(1, 4, 6)).toBe(4);
+            expect(GOVUK.Insights.clamp(10, 4, 16)).toBe(10);
         });
 
     })
+
+    describe("point", function () {
+
+        var point = GOVUK.Insights.point;
+
+        it("should create a point with coordinates", function () {
+            var p = point(123, 456);
+            expect(p.x()).toBe(123);
+            expect(p.y()).toBe(456);
+        });
+
+        it("should create a point with coordinates array", function () {
+            var p = point([123, 456]);
+            expect(p.x()).toBe(123);
+            expect(p.y()).toBe(456);
+        });
+
+        it("should return the distance from another point", function () {
+            var p = point(10, 10);
+
+            expect(p.distanceFrom(p)).toBe(0);
+            expect(p.distanceFrom(point(20, 10))).toBe(10);
+            expect(p.distanceFrom(point(10, 20))).toBe(10);
+            expect(p.distanceFrom(point(11, 11))).toBeCloseTo(1.4142, 4);
+            expect(p.distanceFrom(point(0, 0))).toBeCloseTo(14.1421, 4);
+
+        });
+
+    });
 });
