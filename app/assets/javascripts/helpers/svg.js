9fcec616bcd7769b4ec7074634aad1f5e735e824
Gareth Rogers <grogers@thoughtworks.com>
2013-01-16 16:56:55 +0000

    Disabling graph resizing on safari desktop
    - @gtrogers

--- app/assets/javascripts/helpers/svg.js
+++ app/assets/javascripts/helpers/svg.js
@@ -49,12 +49,20 @@ GOVUK.Insights.svg.translate = function(x, y) {
 }
 
 GOVUK.Insights.svg.supportsResizing = function () {
-    return !($('#wrapper').hasClass("ie9"));
+    // is there a nicer way of doing this?
+    var isSafari = (navigator.userAgent.toLowerCase().indexOf("safari") !== -1) && (navigator.userAgent.toLowerCase().indexOf("chrome") === -1),
+        isMobile = (navigator.userAgent.toLowerCase().indexOf("mobile") !== -1),
+        isDesktopSafari = isSafari && !isMobile;
+    console.log(isDesktopSafari);
+    if ($('#wrapper').hasClass("ie9") ||  isDesktopSafari) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 GOVUK.Insights.svg.resizeIfPossible = function (svg, width, height) {
     if (GOVUK.Insights.svg.supportsResizing()) {
-        console.log("addin' classes", svg.node())
         svg
             .attr("width","100%")
             .attr("height","100%")
