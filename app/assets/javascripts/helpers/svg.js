a4de4532b57d50ee7d064deaa48ae6262ec7a7f9
Gareth Rogers <grogers@thoughtworks.com>
2013-01-21 13:25:29 +0000

    Fixing too-much-whitespace problem on safari
     - hoping this will also work on android.
    
    @gtrogers

--- app/assets/javascripts/helpers/svg.js
+++ app/assets/javascripts/helpers/svg.js
@@ -50,10 +50,7 @@ GOVUK.Insights.svg.translate = function(x, y) {
 
 GOVUK.Insights.svg.supportsResizing = function () {
     // is there a nicer way of doing this?
-    var isSafari = (navigator.userAgent.toLowerCase().indexOf("safari") !== -1) && (navigator.userAgent.toLowerCase().indexOf("chrome") === -1),
-        isMobile = (navigator.userAgent.toLowerCase().indexOf("mobile") !== -1),
-        isDesktopSafari = isSafari && !isMobile;
-    if ($('#wrapper').hasClass("ie9") ||  isDesktopSafari) {
+    if ($('#wrapper').hasClass("ie9")) {
         return false;
     } else {
         return true;
@@ -62,12 +59,15 @@ GOVUK.Insights.svg.supportsResizing = function () {
 
 GOVUK.Insights.svg.resizeIfPossible = function (svg, width, height) {
     if (GOVUK.Insights.svg.supportsResizing()) {
-        svg
-            .attr("width","100%")
-            .attr("height","100%")
-            .style("max-width",width + 'px')
-            .style("max-height",height + 'px');
-    }
+            svg
+                .attr("width","100%")
+                .attr("height","100%")
+                .style("max-width",width + 'px')
+                .style("max-height",height + 'px');
+            d3.select(svg.node().parent)
+                .style("max-width",width + 'px')
+                .style("max-height",height + 'px');
+    } 
 };
 
 GOVUK.Insights.svg.scaleFactor = function (svg, fullWidth) {
