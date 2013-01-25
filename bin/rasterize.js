22267364946362ba5815f93c66b8be3181f8c938
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-01-25 15:04:58 +0000

    Render fonts correctly in SVG graphs
    @robyoung
    @pbadenski

--- bin/rasterize.js
+++ bin/rasterize.js
@@ -32,8 +32,21 @@ if (phantom.args.length < 2 || phantom.args.length > 4) {
             }
         }
 
+        function base64EncodedFont(filename) {
+            var fs = require("fs");
+            var fontFile = fs.open("fonts/" + filename, "r");
+            var base64EncodedFont = "data:application/octet-stream;base64," + btoa(unescape(encodeURIComponent(fontFile.read())))
+        }
+
+        function replace_WOFF_FontsWith_TTF(fontFamily, base64EncodedFont) {
+            var style = $("<style> @font-face { font-family: 'nta'; src: url(" + base64EncodedFont + ") format('truetype'); font-weight: normal; font-style: normal; } @font-face { font-family: 'ntatabularnumbers'; src: url(" + base64EncodedFont + ") format('truetype'); font-weight: normal; font-style: normal; }</style>");
+            $('html > head').append(style);
+        }
+
         function processPage() {
             page.evaluate(hideAnnotationMarkers);
+            replace_WOFF_FontsWith_TTF(base64EncodedFont("nta", "nta.ttf"));
+            replace_WOFF_FontsWith_TTF(base64EncodedFont("ntatabularnumbers", "ntatabularnumbers.ttf"));
             page.clipRect = page.evaluate(getClipRect, selector);
             var saved = page.render(output);
             exit(saved);
