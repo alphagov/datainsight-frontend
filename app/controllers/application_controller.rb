4f2ca9ef8722534947cbdea5f394550277296522
Oliver Spindler <os@oliverspindler.com>
2013-06-05 16:22:37 +0100

    use 'proposition' type main navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # use 'homepage' template to remove global navigation element
+  include Slimmer::Template
+  slimmer_template :homepage
+
   def create_client_api(config)
     client_api_class.new(config)
   end
